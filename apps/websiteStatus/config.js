c5d926bb8d3a123353e13d11dbfd1a8d60c3c696
SiberiaHusky <67987406+SiberiaHusky@users.noreply.github.com>
2023-05-23 13:44:36 +0800

    Add files via upload

---
+++ apps/websiteStatus/config.js
@@ -0,0 +1,25 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'Public Status',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'u1917735-b49736c7850d1714c3069a4e',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '主站 Main site',
+      url: 'https://hooskai.ml/'
+    },
+  ],
+};
